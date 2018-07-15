7f2bf781e1ad922d509d7c45dbc2bf3e642a3345
GossipJie <GossipJie@example.com>
2018-07-15 21:39:52 +0800

    spring boot study

---
+++ src/main/java/com/gossip/xiaofeiji/XiaofeijiApplication.java
@@ -0,0 +1,12 @@
+package com.gossip.xiaofeiji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaofeijiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiaofeijiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gossip/xiaofeiji/XiaofeijiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gossip.xiaofeiji;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XiaofeijiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
